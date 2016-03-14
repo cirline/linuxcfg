4214d8f1dcf97ec59ac0dcdecfca7bce94ea24f5
chenqiwei <chenqiwei@szhklt.com>
2016-03-14 15:23:27 +0800

    simple-tools: add builder, and helper

--- build-tools/build_defconfig.mk
+++ build-tools/build_defconfig.mk
@@ -2,6 +2,10 @@
 install_path		?= $(home_path)/tools
 out_path			?= $(build_path)/out
 
+# tools set
+# tools debug
+tools_debug		?= 0
+
 # tools run
 efs_data_path	?= $(out_path)/efsdata
 efs_mnt_path	?= $(out_path)/efsmnt
